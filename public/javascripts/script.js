a65fd32f0e862fdf919800bb29662958393041a6
goodbreathseth <goodbreathseth@gmail.com>
2019-12-11 05:39:49 +0000

    Have all functionality working

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,18 +1,18 @@
 new Vue({
     el: '#app',
     data: {
-      words: [],
-      touchedCards: [],
-      turn: '',
+        words: [],
+        touchedCards: [],
+        allCards: [],
+        turn: '',
+        index: 0,
+        redCardsLeft: 9,
+        blueCardsLeft: 8,
     },
     beforeMount: function() {
         this.getWords();
         this.getCards();
         this.touchedCards = new Array(25).fill(false);
-        // let url = "/getCards";
-        // fetch(url).then(response => response.json()).then(data => {
-        //     this.cards = data;
-        // });
         this.turn = 'blue';
     },
     methods: {
@@ -22,32 +22,77 @@ new Vue({
             try {
                 let response = await axios.get("/getCards");
                 this.words = response.data;
-            } catch (error) {
+            }
+            catch (error) {
                 console.log(error);
             }
         },
         async getCards() {
             try {
-              let response = await axios.get("/api/getCards");
-              this.allCards = response.data;
-              console.log("got cards:");
-              console.log(this.allCards);
-              return true;
-            } catch (error) {
-              console.log(error);
+                let response = await axios.get("/api/getCards");
+                this.allCards = response.data;
+                console.log("got cards:");
+                console.log(this.allCards);
+                return true;
+            }
+            catch (error) {
+                console.log(error);
             }
         },
-        cardSelected: function( index ) {
-            this.touchedCards[index] = true;
-            console.log("Card " + this.words[ index ] + " was clicked");
-            console.log(this.touchedCards);
+        cardSelected: function(index) {
+            if (this.touchedCards[index] == false) {
+            
+                Vue.set(this.touchedCards, index, true)
+                let variableChanged = false;
+                let tempRedCards = 9;
+                for (let i = 0; i < this.allCards.redCards.length; i++) {
+                    if (this.allCards.redCards[i] == index) {
+                        this.redCardsLeft--;
+                    }
+                }
+               
+                variableChanged = false;
+                let tempBlueCards = 8;
+                for (let i = 0; i < this.allCards.blueCards.length; i++) {
+                    if (this.allCards.blueCards[i] == index) {
+                        this.blueCardsLeft--;
+                    }
+                }    
+            }
+            
+            
+            // console.log("Card " + this.words[index] + " was clicked");
         },
-        isCardTouched: function( index ) {
-            // if (this.touchedCards[index])
-            //     return 'bg-red-400';
-            // else
-            //     return 'hidden';
+        isCardTouched: function(index) {
             return this.touchedCards[index];
+        },
+        getCardColor: function(index) {
+            if (this.touchedCards[index]) {
+                if (index == this.allCards.assassin) {
+                    return 'bg-black'
+                }
+
+                for (let i = 0; i < this.allCards.redCards.length; i++) {
+                    if (this.allCards.redCards[i] == index) {
+                        return 'bg-red-700'
+                    }
+                }
+
+                for (let i = 0; i < this.allCards.blueCards.length; i++) {
+                    if (this.allCards.blueCards[i] == index) {
+                        return 'bg-blue-700'
+                    }
+                }
+
+                for (let i = 0; i < this.allCards.bystanderCards.length; i++) {
+                    if (this.allCards.bystanderCards[i] == index) {
+                        return 'bg-gray-400'
+                    }
+                }
+            }
+            else {
+                return 'bg-orange-200'
+            }
         },
         changeTurn: function() {
             console.log("changed turn")
@@ -76,4 +121,4 @@ new Vue({
                 return 'bg-red-700'
         },
     }, // End of computed
-  })
+})
