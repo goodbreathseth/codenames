2758bb7a9bfab0127d39828aad8203b93f931560
goodbreathseth <goodbreathseth@gmail.com>
2020-04-06 21:26:59 -0700

    Added color change

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -7,10 +7,24 @@ let indexApp = new Vue({
         if (localStorage.team) {
             this.team = localStorage.team;
         }
+        setTimeout(function(){
+            // This hides the address bar:
+            window.scrollTo(0, 1);
+        }, 0);
     },
     watch: {
         team(newTeam) {
             localStorage.team = newTeam;
+        }
+    },
+    computed: {
+        teamColor() {
+            if (this.team == 'blue') 
+                return 'bg-blue-800'
+            else if (this.team == 'red')
+                return 'bg-red-800'
+            else    
+                return 'bg-gray-900'
         }
     }
 });
